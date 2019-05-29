9da79c7a668e27277daf4df643b7bb618dabd1d5
Harshkumar Dave <HDave@CEGEPGIM.LCL>
2019-05-29 11:05:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mad_311work/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.mad_311work;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText edt_name,edt_email,edt_pass;
+    Button btn_sub;
+
+    static String TAG = "My Info";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edt_name = findViewById(R.id.edt_name);
+        edt_email = findViewById(R.id.edt_email);
+        edt_pass = findViewById(R.id.edt_pass);
+
+        btn_sub = findViewById(R.id.btn_submit);
+
+
+        final Toast toast = Toast.makeText(getApplicationContext(),"First Message",Toast.LENGTH_SHORT);
+
+        btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = edt_name.getText().toString();
+                String email = edt_email.getText().toString();
+
+               Log.i(TAG,name);
+               Log.i(TAG,email);
+               Log.i(TAG,edt_pass.getText().toString());
+
+               toast.setText("Second Message!");
+               toast.setDuration(Toast.LENGTH_LONG);
+
+               toast.show();
+
+                //Toast.makeText(getApplicationContext(),"Registration Done!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
